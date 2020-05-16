37727ee1c635da15d88d20e79b302e8f36b23386
Juan Diego Acosta <juandiegoat01@gmail.com>
2020-05-16 15:36:58 -0500

    Updates

---
+++ precache-manifest.6e9a12445031736d66596f7c0ca7ad74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1014072207f776d84d6441f03e9b5db2",
+    "url": "/Rick-And-Morty-Api/index.html"
+  },
+  {
+    "revision": "03a75d9d7c5e26554ef1",
+    "url": "/Rick-And-Morty-Api/static/css/main.956c2466.chunk.css"
+  },
+  {
+    "revision": "83a97a0a89f11315d86c",
+    "url": "/Rick-And-Morty-Api/static/js/2.6081f0c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rick-And-Morty-Api/static/js/2.6081f0c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03a75d9d7c5e26554ef1",
+    "url": "/Rick-And-Morty-Api/static/js/main.b3abcad6.chunk.js"
+  },
+  {
+    "revision": "671b558305fe8a17d2aa",
+    "url": "/Rick-And-Morty-Api/static/js/runtime-main.411cc40d.js"
+  },
+  {
+    "revision": "de82c64b0013c1e2a22f5a8fb26f04f4",
+    "url": "/Rick-And-Morty-Api/static/media/logo.de82c64b.png"
+  }
+]);
